95f72c770c3302974cde58c5fc652aa5b930a0b4
skblaz <skrljblaz@gmail.com>
2017-02-11 11:15:35 +0100

    Working extractor

--- get_bk.py
+++ get_bk.py
@@ -70,61 +70,121 @@ class make_request:
 
         return G
         
-    def execute_query(self,sourceterms,targetterms, maxnodes,grouping):
+    def execute_query(self,sourceterms,targetterms=None, maxnodes=200,grouping=0):
         
-        # ##additional source and target nodes can be added.
-
-        # #params2 = urllib.parse.urlencode({'sourceTerms': 'InterPro:IPR011364, EntrezGene:672','graph_type': 'json'}).encode('utf-8')
-
-        # params = urllib.parse.urlencode({'database': self.databases['biomine'][0],
-        #                             'sourceTerms': 'EntrezGene:27086',
-        #                             'targetTerms': 'EntrezGene:93986',
-        #                             'maxnodes': 500,
-        #                             'grouping': 5,
-        #                                   'graph_type': 'json'}).encode("utf-8")
-
-
-        # json_graph =  json.loads(urllib.request.urlopen(self.bm_api, params).read().decode())['graph']
         
-        # ## save for possible further use..
-        # print ("Data obtained, constructing the graph..")
-        # nodes = json.loads(json_graph)['nodes']
-        # edges = json.loads(json_graph)['links']
-        # ## colors
-
-        # ## lets create a graph..
-
-        # G = nx.Graph()
-
-        # ## those are the names..
-        # labels = {}
-        # for id,node in enumerate(nodes):
-        #     #print (id, node['id'])
-        #     G.add_node(id)
-        #     labels[id] = node['id']
-        # for id,edge in enumerate(edges):
-        #     #print (id, edge['source'],edge['target'])
-        #     G.add_edge(int(edge['source']),int(edge['target']))
-            
+        print ("Search executed..")
         
-        # print ("Finished")
-        # pos = nx.spring_layout(G)
-        # nx.draw(G,pos)
-        # nx.draw_networkx_labels(G,pos,labels,font_size=16)
-        # plt.show()
+        ## decide on search type
 
-        # return G
+        if targetterms == None:
+            params = urllib.parse.urlencode({'database': self.databases['biomine'][0],
+                                             'sourceTerms': sourceterms,
+                                             'maxnodes': maxnodes,
+                                             'grouping': grouping,
+                                             'graph_type': 'json'}).encode("utf-8")
+        else:
+            params = urllib.parse.urlencode({'database': self.databases['biomine'][0],
+                                             'sourceTerms': sourceterms,
+                                             'targetTerms': targetterms,
+                                             'maxnodes': maxnodes,
+                                             'grouping': grouping,
+                                             'graph_type': 'json'}).encode("utf-8")
+
         
+        json_graph =  json.loads(urllib.request.urlopen(self.bm_api, params).read().decode())['graph']
+        
+        ## save for possible further use..
+        print ("Data obtained, constructing the graph..")
+        nodes = json.loads(json_graph)['nodes']
+        edges = json.loads(json_graph)['links']
+        ## colors
 
+        ## lets create a graph..
 
-    ## convert it to some sort of networkx structure..
+        G = nx.Graph()
+
+        ## those are the names..
+        labels1 = {}
+
+        for id,node in enumerate(nodes):
+
+            if node['organism'] == 'hsa':
+
+                G.add_node(id, name=node['id'], degree=node['degree'], spec=node['organism'], color = 'r')
+
+            else:
+                G.add_node(id, name=node['id'], degree=node['degree'], spec=node['organism'], color = 'g')
+
+            labels1[id] = node['id']
+
+        for id,edge in enumerate(edges):
+
+            G.add_edge(int(edge['source']),int(edge['target']), weight = edge['reliability'])
+                    
+        ## color according to db entry at least.
+
+        edgesG = G.edges()
+        nodesG = G.nodes(data=True)
+        
+        ## assign values to the object for further use
+
+        self.graph_node_degree = [int(u[1]['degree']) for u in nodesG]
+        self.graph_node_colors = [u[1]['color'] for u in nodesG]
+        self.graph_weights = [G[u][v]['weight'] for u,v in edgesG]
+        self.graph = G        
+        self.labels = labels1
+        self.pos = nx.spring_layout(G)
+        #self.pos = nx.circular_layout(G)
+        return G
+
+    def draw_graph(self, labs = False, weights = True, fsize = 10):
+        
+        nsize = [deg*0.1 for deg in self.graph_node_degree]
+
+        if weights == False:
+            if labs == True:
+                nx.draw(self.graph,self.pos,node_size=nsize, node_color = self.graph_node_colors)
+                nx.draw_networkx_labels(self.graph,self.pos,self.labels,font_size=16)
+                plt.show()
+            else:
+                nx.draw(self.graph,self.pos,node_size=nsize,node_color = self.graph_node_colors)
+                nx.draw_networkx_labels(self.graph,self.pos,font_size=16)
+                plt.show()
+        else:
+            if labs == True:
+                nx.draw(self.graph,self.pos, width=self.graph_weights,node_size=nsize,node_color = self.graph_node_colors)
+                nx.draw_networkx_labels(self.graph,self.pos,self.labels,font_size=fsize)
+                plt.show()
+            else:
+                nx.draw(self.graph,self.pos, width=self.graph_weights,node_size=nsize,node_color = self.graph_node_colors)
+                nx.draw_networkx_labels(self.graph,self.pos,font_size=fsize)
+                plt.show() 
+
+def read_example_data(max):
+
+    outlist = []
+    outlist2 = []
+
+    with open("data/cancer.list") as cl:
+        for line in cl:
+           outlist.append("UniProt:"+line.replace("\n",""))
+
+    with open("data/alzheimer.list") as cl:
+        for line in cl:
+           outlist2.append("UniProt:"+line.replace("\n",""))
+
+    return (",".join(outlist[1:max]),",".join(outlist2))
+
 
 if __name__ == '__main__':
+    
+    source, target = read_example_data(200)
 
     ## init a request
     request = make_request()
+    
+    ## this returns graph for further reduction use..
+    request.execute_query(source)
+    request.draw_graph()
 
-    ## some tests
-    #request.get_info()
-    #request.list_db()
-    print(request.test_query())
