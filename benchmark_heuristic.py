61a5e726e6277bc99439c8fad7af7957c9a0fd93
SkBlaz <skrljblaz@gmail.com>
2017-06-11 12:35:21 +0200

    Heuristic tests

--- benchmark_heuristic.py
+++ benchmark_heuristic.py
@@ -2,20 +2,25 @@
 import networkx as nx
 import numpy as np
 from g2o import *
+import time
+import sys
 
 heuristics = ["degree","pagerank_scipy","eigenvector","communicability","closeness","betweenness"]
 input_graphs = ["graph_datasets/snps.gpickle","graph_datasets/biominetestgraph.gpickle"]
 
 results = []
-for h in heuristics:
-    for inp in input_graphs:
-        print("dataset: ",inp, " and heuristic: ",h)
-        G = nx.read_gpickle(inp)
-        res_graph = g2o(G,90,1,h)
-        n_nodes = len(res_graph.nodes())
-        n_edges = len(res_graph.edges())
-        results.append((inp,h,n_nodes,n_edges))
+for j in range(0,10,1):
+    for h in heuristics:
+        for inp in input_graphs:
+            print("dataset: ",inp, " and heuristic: ",h)
+            start = time.time()
+            G = nx.read_gpickle(inp)
+            res_graph = g2o(G,90,1,h)
+            finaltime = time.time()-start
+            n_nodes = len(res_graph.nodes())
+            n_edges = len(res_graph.edges())
+            results.append((inp,h,n_nodes,n_edges,finaltime))
 
 import json
-with open('OUTPUT/speed.json', 'w') as outfile:
+with open(sys.argv[1], 'w') as outfile:
     json.dump(results, outfile)
