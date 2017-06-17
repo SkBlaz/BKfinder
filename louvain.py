435af4b8056cb4059789949d695a857a816d4ecb
SkBlaz <skrljblaz@gmail.com>
2017-06-17 07:29:44 +0200

    simulations

--- louvain.py
+++ louvain.py
@@ -4,7 +4,10 @@ import matplotlib.pyplot as plt
 
 #G = nx.random_graphs.powerlaw_cluster_graph(10000, 1, .4)
 #G = nx.newman_watts_strogatz_graph(1000, 15, 0.02, seed=None)
-G = nx.barabasi_albert_graph(10000, 1, seed=None)
+G = nx.barabasi_albert_graph(1000, 1, seed=None)
+#G = nx.watts_strogatz_graph(1000,5,0.04)
+
+
 Gx = nx.Graph()
 #G = nx.read_gpickle("graph_datasets/snpsstep1.gpickle")
 
